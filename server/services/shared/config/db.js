410e24a055ef49a4a4d99a6a0225ce170eca07e3
vermavikasakash <vikas26061995@gmail.com>
2026-06-15 18:36:42 +0530

    mogodb image added

--- server/services/shared/config/db.js
+++ server/services/shared/config/db.js
@@ -8,7 +8,7 @@ const connect = async () => {
   }
 
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(uri);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
